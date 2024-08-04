957d32813a9f5677ecd6fb4737c1afec350bc2ae
junkbreak <75923750+lyy061110@users.noreply.github.com>
2024-08-04 12:31:32 +0800

    Add files via upload

---
+++ Project3/Project3/project3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf("H      H        A\n");
+	printf("H      H       A A\n");
+	printf("HHHHHHHH      AAAAA\n");
+	printf("H      H     A     A\n");
+	printf("H      H    A       A\n");
+	return 0;
+}
